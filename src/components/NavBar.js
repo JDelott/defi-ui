7ea50b2a11e5b5bfed18e6cfc1ded353b6ff61f9
Jacob Delott <jldelott@gmail.com>
2023-12-14 20:24:05 -0800

    setup and NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+const NavBar = () => {
+  return (
+    <div className='header'>
+        <div className='conatiner'>
+             <h1>De<span className='primary'>Fi</span></h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default NavBar
