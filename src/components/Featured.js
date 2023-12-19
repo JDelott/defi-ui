ca02fbb8965ee70ed06cf31be226500bd506c87b
Jacob Delott <jldelott@gmail.com>
2023-12-18 18:38:23 -0800

    featured complete

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -46,12 +46,12 @@ const Featured = () => {
 
                 {data[0].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown  className='icon'/>
                         {data[0].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[0].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
@@ -72,12 +72,12 @@ const Featured = () => {
 
                 {data[1].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown className='icon'/>
                         {data[1].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[1].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
@@ -98,12 +98,12 @@ const Featured = () => {
 
                 {data[2].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown className='icon'/>
                         {data[2].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[2].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
@@ -124,12 +124,12 @@ const Featured = () => {
 
                 {data[3].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown className='icon'/>
                         {data[3].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[3].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
@@ -150,12 +150,12 @@ const Featured = () => {
 
                 {data[4].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown className='icon'/>
                         {data[4].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[4].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
@@ -176,12 +176,12 @@ const Featured = () => {
 
                 {data[5].price_change_percentage_24h < 0 ? (
                     <span className='red'>
-                        <FiArrowDown />
+                        <FiArrowDown className='icon'/>
                         {data[5].price_change_percentage_24h.toFixed(2)}%
                     </span>
                 ) : (
                     <span className='green'>
-                    <FiArrowUpRight />
+                    <FiArrowUpRight className='icon'/>
                     {data[5 ].price_change_percentage_24h.toFixed(2)}
                     </span>
                 )}
