57b5886302c3195a4f9dcafc741c5607b42c54f6
Jacob Delott <jldelott@gmail.com>
2023-12-18 19:36:40 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from './components/NavBar'
 import { Hero } from "./components/Hero";
 import Featured from "./components/Featured";
+import Signup from "./components/Signup";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar/>
       <Hero />
       <Featured />
+      <Signup />
+      <Footer />
     </>
   );
 }
